3d7a326e64df4a217423b7b39bcd423ea9ed0c71
yarik2215 <40182728+yarik2215@users.noreply.github.com>
2019-12-12 15:22:23 +0200

    add header file

---
+++ Commander.h
@@ -0,0 +1,117 @@
+/*TODO: сделать диспетчер задач, системный таймер и очередь в виде кольцевого буфера (продумать мьютексы)*/
+
+#pragma once
+
+#include <Arduino.h>
+#include <stdint.h>
+
+//#define QUEUE_SIZE 100
+#define FREQ_TIMER 16000UL
+
+struct Task{
+    Task(void(*f)()) : func(f) {}
+    void (*func)() = nullptr;
+    uint64_t timer = 0;
+    bool mutex = false;
+    Task *nextTask = nullptr;
+};
+
+class Commander{
+public:
+    Commander() = default;
+    ~Commander() = default;
+    void begin();
+    void check(); /*проверяет очередь и убирает из нее задачи после их выполнения*/
+    bool moveToQueue(Task* task, uint64_t time); 
+    void updateTimer();
+
+private:
+    Task *head = nullptr;
+};
+
+
+void Commander::begin()
+{
+    cli();
+    TCCR1B = 0b001;  //clock div1
+    TCNT1 = 0;
+    TCCR1A = 0;
+    OCR1A = FREQ_TIMER;
+    TIMSK1 |= (1 << OCIE1A);
+    SREG |= (1<<SREG_I);
+    sei();
+}
+
+
+void Commander::check()
+{
+    if(head == nullptr)
+        return;         //пустой список
+
+    Task *prevTask = nullptr;
+    Task *curTask = head;
+    while(curTask != nullptr) {
+        //выполнение задачи по истечению таймера и если функция не заблокирована
+        Task *nextTask = curTask->nextTask;
+
+        if(curTask->timer == 0 && curTask->mutex == false) {
+            if(prevTask == nullptr) {
+                head = curTask->nextTask;
+            }
+            else {
+                prevTask->nextTask = curTask->nextTask;
+                //curTask = prevTask;
+            }
+            curTask->func();
+        }
+        else {
+            prevTask = curTask;
+        }
+        curTask = nextTask;
+    }
+}
+
+
+bool Commander::moveToQueue(Task* task, uint64_t time)
+{
+    cli();
+    task->timer = time;
+    task->nextTask = nullptr;
+    sei();
+    
+    // Serial.println("Move task");
+    if(head == nullptr) {
+        head = task;
+        // Serial.println("Move to head");
+        return;
+    }
+    Task *curTask = head;
+    while (curTask->nextTask != nullptr)
+    {
+        curTask = curTask->nextTask;
+    }
+    curTask->nextTask = task;
+    // Serial.println("Move to end");
+} 
+
+
+void Commander::updateTimer()
+{
+    Task *curTask = head;
+    while(curTask != nullptr){
+        if(curTask->timer != 0) {
+            curTask->timer--;
+        }
+        curTask = curTask->nextTask;
+    }
+}
+
+
+Commander commander;
+
+ISR(TIMER1_COMPA_vect)
+{
+  TCNT1 = 0;
+  sei();
+  commander.updateTimer();
+}
